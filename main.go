e614e759a9dcd8bbe57395749d9f18d908bcc1d4
shinpei <shinpei.nakata@gmail.com>
2015-06-04 12:06:52 +0900

    using struct for pushing structured data

--- main.go
+++ main.go
@@ -7,33 +7,48 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
+	"runtime"
 )
 
 type Data struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	TextCount int    `json:"text_count"`
 }
 
 func main() {
+	cpus := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpus)
 
-	b, err := json.Marshal(`{"add": { "doc" : { "id" : "hoge", "title": "hi"} }}`)
+	d := []Data{{
+		Id:        "hige",
+		Title:     "hoge",
+		Text:      "fuga",
+		TextCount: 12,
+	},
+	}
+
+	b, err := json.Marshal(d)
 	ss := string(b[:])
-	print(ss)
+	println(ss)
 	if err != nil {
 		panic(err)
 	}
-	respB, err := Post("http://localhost:8983/solr/update?wt=json&commit=true", &b)
+
+	respB, err := Post("http://localhost:8983/solr/update/json?commit=true", b)
+
 	if err != nil {
 		panic(err)
 	}
-	var datas []Data
+	var datas interface{}
 	err = json.Unmarshal(respB, &datas)
 
 	if err != nil {
 		s := string(respB[:])
 		println(s)
 	} else {
-		fmt.Printf("%%x\n", datas[0])
+		fmt.Printf("%x\n", datas)
 	}
 }
 
@@ -50,9 +65,9 @@ func Get(url string) ([]byte, error) {
 	return body, nil
 }
 
-func Post(url string, payload *[]byte) ([]byte, error) {
+func Post(url string, payload []byte) ([]byte, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, bytes.NewReader(*payload))
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
 	req.Header.Add("Content-type", "application/json")
 	dump, _ := httputil.DumpRequestOut(req, true)
 	fmt.Printf("%s", dump)
